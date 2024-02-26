b24f8d76b84fc3e1d8c6715bb2808ff731bb77f2
jboy138 <155222254+jboy138@users.noreply.github.com>
2024-02-26 00:26:40 +0000

    Working with Trading bots OANADA API

---
+++ defs.py
@@ -0,0 +1,7 @@
+API_KEY = "12620da134a9a4023556d4bfb725105e-47289560715e94f8a3df59577462f572"
+ACCOUNT_ID = "101-004-28440631-001"
+OANDA_URL = "https://api-fxpractice.oanda.com/v3"
+
+SECURE_HEADER = {
+    'Authorization' : f'Bearer {API_KEY}'
+}
